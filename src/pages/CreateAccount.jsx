6ba808394c8e7d4a24a1533202de7be8dc8cf74b
sachin dubey <134124435+sachin9336@users.noreply.github.com>
2025-03-26 02:31:53 -0400

    Update CreateAccount.jsx

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -15,7 +15,7 @@ const CreateAccount = () => {
     setMessage("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://sachusicplayer.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
